621a782be901471a861091d008ad749801cec108
Nguyễn Thanh Đa <147078224+ThanhDa305@users.noreply.github.com>
2024-10-17 15:31:33 +0700

    Add files via upload

---
+++ buoi3/major/major_form.php
@@ -0,0 +1,11 @@
+<!DOCTYPE HTML>
+<html>
+<body>
+<form action="major_add.php" method="post">
+id: <input type="text" name="id"><br>   
+Name: <input type="text" name="name_major"><br>
+
+<input type="submit">
+</form>
+</body>
+</html>
